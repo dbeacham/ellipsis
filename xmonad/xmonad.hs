e70f26b300a9319cb3e8a5493a601598ab3fb0e6
David Beacham <dbeacham@dbeacham.co.uk>
2015-11-20 16:50:58 +0000

    Update

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -15,7 +15,7 @@ import System.IO
 --definitions
 myTerminal = "gnome-terminal"
 myWorkspaces = [
-  "1:main","2:mail","3:chat","4:web","5:browse","6:dev","7:audio", "8:ms"
+  "1:main","2:mail","3:chat","4:web","5:browse","6:dev","7:audio", "8:ms", "9:trash"
   ]
 
 myManageHook = composeAll . concat $
@@ -24,22 +24,26 @@ myManageHook = composeAll . concat $
    , [ resource =? b --> doF (W.shift "3:chat") | b <- myClassChatShifts ]
    , [ className =? a --> viewShift "7:audio" | a <- myClassAudioShifts ]
    , [ className =? a --> viewShift "8:ms" | a <- myClassMSShifts ]
+   , [ className =? a --> doF (W.shift "9:trash") | a <- myRebootShifts ]
    ]
   where viewShift = doF . liftM2 (.) W.view W.shift
         myClassMailShifts = ["Icedove", "Thunderbird"]
-        myClassWebShifts = ["Chromium", "Chromium-browser", "Iceweasel", "Firefox", "Google-chrome", "Google-chrome-unstable"]
+        myClassWebShifts = [ "Chromium", "Chromium-browser", "Iceweasel", "Firefox", "Google-chrome", "Google-chrome-unstable"
+                           , "chromium", "chromium-browser", "iceweasel", "firefox", "google-chrome", "google-chrome-unstable"
+                           ]
         myClassChatShifts = ["Pidgin", "Skype"]
         myClassAudioShifts = ["Gpodder", "Spotify"]
         myClassMSShifts = ["Remminna", "VirtualBox"]
+        myRebootShifts = ["Zenity"]
 
 main = do
-    mapM spawnPipe
+    mapM_ spawnPipe
          [
            "xscreensaver"
          , "feh --bg-center $HOME/.xmonad/bg/haskell-pattern.png"
          --, "xloadimage -onroot -fullscreen $HOME/.xmonad/bg/haskell-pattern.png"
-         , "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand false --width 10 --transparent true --tint 0x000 --height 12 --alpha 0 --padding 1"
-         , "nm-applet --sm-disable"
+         -- , "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand false --width 10 --transparent true --tint 0x000 --height 12 --alpha 0 --padding 1"
+         -- , "nm-applet --sm-disable"
          ]
     xmproc <- spawnPipe "/usr/bin/xmobar"
     xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
