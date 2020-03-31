e26966543b4ae1bbb1775328b32e579f5dc3fe62
David Beacham <dbeacham@dbeacham.co.uk>
2020-03-31 16:05:25 +0100

    Install script + vim updates

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+ln -s $PWD/xmonad $HOME/.xmonad
+ln -s $PWD/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $PWD/git/gitconfig $HOME/.gitconfig
+ln -s $PWD/git/gitignore $HOME/.gitignore
+ln -s $PWD/python/pyrc $HOME/.pyrc
+ln -s $PWD/nvim $HOME/.config/nvim
+ln -s $PWD/ghci/ghci $HOME/.ghci
