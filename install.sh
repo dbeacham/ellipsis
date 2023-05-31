591408d17f31d03fbde0de7e15bafcbe401edda6
David Beacham <dbeacham@dbeacham.co.uk>
2023-05-31 13:06:48 +0100

    Fix xmonad installation

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-ln -s $PWD/xmonad $HOME/.xmonad
-ln -s $PWD/xmonad/xmobarrc $HOME/.xmobarrc
+ln -s $PWD/x/monad $HOME/.xmonad
+ln -s $PWD/x/monad/xmobarrc $HOME/.xmobarrc
 ln -s $PWD/tmux/tmux.conf $HOME/.tmux.conf
 ln -s $PWD/git/gitconfig $HOME/.gitconfig
 ln -s $PWD/git/gitignore $HOME/.gitignore
 ln -s $PWD/python/pyrc $HOME/.pyrc
 ln -s $PWD/nvim $HOME/.config/
 ln -s $PWD/ghci/ghci $HOME/.ghci
-ln -s $PWD/xsession/xsession $HOME/.xsession
-ln -s $PWD/xsession/Xresources $HOME/.Xresources
+ln -s $PWD/x/session/xsession $HOME/.xsession
+ln -s $PWD/x/session/Xresources $HOME/.Xresources
